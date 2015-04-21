162afefd74f4ccd038ce14a328e1fc02595ecab7
Julian van Riet <vanrietjulian@gmail.com>
2015-04-21 08:58:47 -0700

    Milestone 9

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 
 <html>
 	<head>
-		<title> Julian's To-Do List 2 </title>
+		<title> Julian's To-Do List </title>
 		<link rel="stylesheet" type="text/css" href="css/main.css">
 	</head>
 	<body>
 		<div class="wrap">
-		<div class="task-list">
+			<div class="task-list">
 				<ul>
 					<?php require("includes/connect.php"); ?>
 				</ul>
@@ -17,4 +17,20 @@
 			</form>
 		</div>
 	</body>
+	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script>
+		add_task();
+		function add_task() {
+			$('.add-new-task').submit(function() {
+				var new_task = $('.add-new-task input[name=new-task]').val();
+				if (new_task != '') {
+					$.post('includes/add-task.php', { task: new_task }, function (data) {
+						$('add-new-task input[name=new-task]').val();
+						$(data).appendTo('task-list ul').hide.fadeIn();
+					});
+				}
+				return false;
+			});
+		}
+	</script>
 </html>
