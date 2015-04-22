c7d45998f06b996707fd50005bd71266f977dc0c
Julian van Riet <vanrietjulian@gmail.com>
2015-04-22 09:00:31 -0700

    Milestone 16

--- index.php
+++ index.php
@@ -22,10 +22,15 @@
 								while($row = $result->fetch_assoc()) {
 									$task_id = $row['id'];
 									$task_name = $row['task'];
+
+									echo '<li> 
+									<span>' . $task_name . '</span>
+									<img id="' . $task_id . '" class="delete-button" width="10px" src="images/close.svg"/>
+									</li>';
 								}
 							}
 						}
-						
+
 					?>
 				</ul>
 			</div>
